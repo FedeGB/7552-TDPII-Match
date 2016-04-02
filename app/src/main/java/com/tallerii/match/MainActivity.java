14db4573f9f1ad0b7b66301845ceb45b0b492baf
fedegb <fedeagb@gmail.com>
2016-04-02 17:46:36 -0300

    Added android manifest for network connection. Added some of the request login logic.

--- app/src/main/java/com/tallerii/match/MainActivity.java
+++ app/src/main/java/com/tallerii/match/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tallerii.match;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
@@ -11,6 +15,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -60,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
         // Para llamar a otra actividad con la data entrante
         // Ver si realmente el modulo que se va a comunicar con el server
         // es un activity o que se usa
-        /*Intent intent = new Intent(this, ServerCommunication.class);
+        /*Intent intent = new Intent(this, HttpActivity.class);
         EditText userloginEdit = (EditText) findViewById(R.id.user_login);
         EditText userpassEdit = (EditText) findViewById(R.id.user_pass);
         String userlogin = userloginEdit.getText().toString();
@@ -68,5 +74,51 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_USERLOGIN, userlogin);
         intent.putExtra(EXTRA_USERPASS, userpass);
         startActivity(intent);*/
+        // Gets the URL from the UI's text field.
+        //String stringUrl = urlText.getText().toString();
+        EditText userloginEdit = (EditText) findViewById(R.id.user_login);
+        EditText userpassEdit = (EditText) findViewById(R.id.user_pass);
+        String userlogin;
+        String userpass;
+        try {
+            userlogin = userloginEdit.getText().toString();
+            userpass = userpassEdit.getText().toString();
+        } catch(NullPointerException ex) {
+            userlogin = "";
+            userpass = "";
+        }
+        ConnectivityManager connMgr = (ConnectivityManager)
+        getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            if(userlogin.isEmpty() || userpass.isEmpty()) {
+                Snackbar.make(view, "Email or Password can't be empty.", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            } else {
+                new HttpLoginRequestTask().execute(userlogin, userpass);
+            }
+        } else {
+            Snackbar.make(view, "No network connection available.", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        }
+    }
+
+    private class HttpLoginRequestTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+        //    try {
+      //          return ""/*downloadUrl(urls[0])*/;
+    //        } catch (IOException e) {
+  //              return "Unable to retrieve web page. URL may be invalid.";
+//            }
+            return "";
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            //textView.setText(result);
+        }
     }
 }
