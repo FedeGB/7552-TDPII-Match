a57c68159f63bcf7e620158b7206adc6f7c321f2
demianlopez <demianlopez1@gmail.com>
2016-04-20 11:48:00 -0300

    Logica de edicion en EditTextActivity y comenzando en EditPerfilActivity #3

--- app/src/main/java/com/tallerii/match/EditPerfilActivity.java
+++ app/src/main/java/com/tallerii/match/EditPerfilActivity.java
@@ -1,10 +1,70 @@
 package com.tallerii.match;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.SynchronousQueue;
 
 public class EditPerfilActivity extends AppCompatActivity {
+
+    enum InterestCodeEnum {
+        MUSIC(1, "music"), MUSICBAND(2, "music/band"), SPORT(3, "sport"), SEX(4, "sex"), OUTDOORS(5, "outdoors"), TRAVEL(6, "travel"), FOOD(7, "food");
+
+        private List<String> interestList;
+        private int numVal;
+        private String name;
+
+        InterestCodeEnum(int numVal, String name){
+            this.numVal = numVal;
+            this.name = name;
+            this.interestList = new ArrayList<String>();
+        }
+
+        public int getNumVal(){
+            return numVal;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public void addToList(String name){
+            interestList.add(name);
+
+            Iterator<String> i = interestList.iterator();
+            while (i.hasNext()){
+                System.out.println(i.next());
+            }
+        }
+
+        static public InterestCodeEnum getFromValue(int numVal){
+            for(InterestCodeEnum iC: InterestCodeEnum.values()){
+                if(iC.getNumVal() == numVal){
+                    return iC;
+                }
+            }
+
+            return null;
+        }
+
+        static public InterestCodeEnum getFromName(String name){
+            for(InterestCodeEnum iC: InterestCodeEnum.values()){
+                if(iC.getName().compareTo(name) == 0){
+                    return iC;
+                }
+            }
+
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +72,34 @@ public class EditPerfilActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_perfil);
     }
 
-    private void buildContent(){
+    public void OnAddInterest(View view){
+        String interestName = view.getTag().toString();
+        int interestCode = InterestCodeEnum.getFromName(interestName).getNumVal();
 
+        Intent i = new Intent(this, EditTextActivity.class);
+        i.putExtra("Title", "Add interest: " + interestName);
+        startActivityForResult(i, interestCode);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(resultCode == Activity.RESULT_CANCELED){
+            return;
+        }
+
+        String resultText = data.getStringExtra("result");
+
+        addToInterest(requestCode, resultText);
+    }
+
+    private void addToInterest(int interestCode, String interest){
+        InterestCodeEnum.getFromValue(interestCode).addToList(interest);
+        buildInterestView();
+    }
+
+    private void buildInterestView(){
+  
+    }
+
 }
--- app/src/main/java/com/tallerii/match/EditTextActivity.java
+++ app/src/main/java/com/tallerii/match/EditTextActivity.java
@@ -1,7 +1,12 @@
 package com.tallerii.match;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTextActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class EditTextActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_text);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null) {
+            String title = extras.getString("Title");
+            System.out.println(title);
+            TextView tvTitle = (TextView) findViewById(R.id.ET_TV_Title);
+            tvTitle.setText(title);
+        }
+    }
+
+    public void OnAccept(View view){
+        Intent returnIntent = new Intent();
+
+        EditText etString = (EditText) findViewById(R.id.ET_ET_stringText);
+        returnIntent.putExtra("result", etString.getText().toString());
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();
+    }
+
+    public void OnCancel(View view){
+        Intent returnIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, returnIntent);
+        finish();
     }
 }
