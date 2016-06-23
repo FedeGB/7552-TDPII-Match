582ed3660ab35a14c8a688671a81f588a433dbf9
demianlopez <demianlopez1@gmail.com>
2016-06-23 02:01:52 -0300

    Cambio al timing de update a 1 segundo

--- app/src/main/java/com/tallerii/match/core/TimeEvent.java
+++ app/src/main/java/com/tallerii/match/core/TimeEvent.java
@@ -8,7 +8,7 @@ import com.tallerii.match.core.query.QueryListener;
  * Created by demian on 22/06/16.
  */
 public class TimeEvent implements Runnable {
-    private final int DELAY_TIME = 10000;
+    private final int DELAY_TIME = 1000;
 
     Handler handler = new Handler();
     QueryListener listener;
